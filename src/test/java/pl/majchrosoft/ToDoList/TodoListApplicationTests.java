940c0e441ca708de6e885d03c0dc897b9f19f080
lm1 <lukasz.majcher@softnet.com.pl>
2019-11-22 11:15:07 +0100

    init commit

---
+++ src/test/java/pl/majchrosoft/ToDoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.majchrosoft.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
