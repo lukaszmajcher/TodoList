4f6e90591cd97d6fc9804e725252059934596ece
lm1 <lukasz.majcher@softnet.com.pl>
2019-12-11 10:55:27 +0100

    lesson 3 - delete taske

--- src/main/java/pl/majchrosoft/ToDoList/tasks/boundary/TasksViewConroller.java
+++ src/main/java/pl/majchrosoft/ToDoList/tasks/boundary/TasksViewConroller.java
@@ -1,11 +1,16 @@
 package pl.majchrosoft.ToDoList.tasks.boundary;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.majchrosoft.ToDoList.tasks.control.TasksService;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class TasksViewConroller {
@@ -15,6 +20,20 @@ public class TasksViewConroller {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("tasks", tasksService.fetchAll());
+        model.addAttribute("newTask", new CreateTaskRequest());
         return "home";
+    }
+
+    @PostMapping("/tasks")
+    public String addTask(@ModelAttribute("newTask") CreateTaskRequest request) {
+        log.info("Dodaje zadanie ...");
+        tasksService.addTask(request.title, request.description);
+        return "redirect:/";
+    }
+
+    @PostMapping("/tasks/delete/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        tasksService.deleteTask(id);
+        return "redirect:/";
     }
 }
